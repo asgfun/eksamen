27839f22c1b45aa16c08849638891399e4053964
asgfun <57358202+asgfun@users.noreply.github.com>
2021-06-22 14:19:18 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,3 +23,14 @@ let arr = ['bug', 'cat', 'dog', 'flea', 'bat', 'hat', 'rat'];
 let newArr = arr.filter(element => element >= element.indexOf(3));
     
 console.log(newArr);
+
+
+
+
+public class Test {
+    public static void main(String args[]) {
+       String str = new String("hi welcome to Tutorialspoint");
+       int index = str.indexOf('w');
+       System.out.println("Index of the letter w :: "+index);
+    }
+ }

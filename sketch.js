64eabdd5ecf7c195ebc8da47745ec47bc7322420
asgfun <57358202+asgfun@users.noreply.github.com>
2021-06-22 14:09:19 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,15 @@
-var  tal = 5; 
+const letters = ['aaa', 'aab', 'abc'];
+const result = letters.map(e=>e.toLocaleLowerCase()).includes('d');
+console.log(result + letters);
+
+const array1 = ['aaa', 'aab', 'abc'];
+
+const found = array1.find(letter => letter > aab);
+
+console.log(found);
+
+let arr = ['bug', 'cat', 'dog', 'flea', 'bat', 'hat', 'rat'];
+
+let newArr = arr.filter(element => element >= element.indexOf(3));
+    
+console.log(newArr);

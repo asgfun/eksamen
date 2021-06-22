dad6aa09593e4352c1e9875d4c5e00e27f3d34dd
asgfun <57358202+asgfun@users.noreply.github.com>
2021-06-22 14:15:12 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
 const letters = ['aaa', 'aab', 'abc'];
-const result = letters.map(e=>e.toLocaleLowerCase()).includes('d');
-console.log(result + letters);
+const result = letters.map(e=>e.toLocaleLowerCase()).includes('aab');
+console.log(result);
+
+
+
+
 
 const array1 = ['aaa', 'aab', 'abc'];
 
-const found = array1.find(letter => letter > aab);
+const found = array1.find(letter => letter > b);
 
 console.log(found);
+
+
+
+
+
+
 
 let arr = ['bug', 'cat', 'dog', 'flea', 'bat', 'hat', 'rat'];
 

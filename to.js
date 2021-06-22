0543c8cdadc497481d4bb9022d041e68f920d9f1
asgfun <57358202+asgfun@users.noreply.github.com>
2021-06-22 14:16:37 +0200

    Create to.js

---
+++ to.js
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String args[]) {
+       String str = new String("hi welcome to Tutorialspoint");
+       int index = str.indexOf('w');
+       System.out.println("Index of the letter w :: "+index);
+    }
+ }
